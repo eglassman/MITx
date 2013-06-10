439d8b6fa2fd9fea5ec41a1acb7070e91b757c34
Elena Glassman <elena.glassman@gmail.com>
2013-06-10 13:37:34 -0400

    fixed but implemented differently

--- day1.js
+++ day1.js
@@ -1,7 +1,23 @@
-function calculate() {
-                var input = $('#text1:first'); //texting comments
-                var val = input.val();
-                var output = $('#text1_out:first');
-                output.text(val);
-                console.log(val);
-            }
+function calculate(text) {
+    var pattern = /\d+|\+|\-|\*|\/|\(|\)/g;
+    var tokens = text.match(pattern);
+    return JSON.stringify(tokens);
+}
+            
+            
+function setup_calc(div) {
+    var input = $('<input></input>',{type: "text", size: 50})
+    var output = $('<div></div>');
+    var button = $('<button>Calculate</button>');
+    button.bind("click",function (){
+        output.text(String(calculate(input.val())));
+    });
+    $(div).append(input,button,output);
+}
+            
+$(document).ready(function (){
+    $('.calculator').each(function(){
+        //this refers to the <div> with class calculator
+        setup_calc(this);
+    });
+});
